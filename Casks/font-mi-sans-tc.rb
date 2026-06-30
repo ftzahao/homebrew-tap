2bff4e76558f8db939a9c9d07e6e47201dd055fa
ftzahao <ftzahao@icloud.com>
2026-06-30 13:38:06 +0800

    清理 font-mi-sans.rb 中已注释的 OTF、WOFF 和 WOFF2 字体引用，并移除冗余注释

---
+++ Casks/font-mi-sans-tc.rb
@@ -0,0 +1,26 @@
+cask "font-mi-sans-tc" do
+  version "4.003"
+  sha256 "417831f033601f1738fefb2ce9158586d0143385a9dd0c4cfa985bcc16c7443b"
+
+  url "https://hyperos.mi.com/font-download/MiSans_TC.zip",
+      verified: "hyperos.mi.com"
+  name "MiSans TC"
+  desc "MiSans TC 繁体中文"
+  homepage "https://hyperos.mi.com/font/details/tc"
+
+  livecheck do
+    cask "font-mi-sans-tc"
+  end
+
+  font "MiSans TC/ttf/MisansTC-Regular.ttf"
+  font "MiSans TC/ttf/MisansTC-Bold.ttf"
+  font "MiSans TC/ttf/MisansTC-Light.ttf"
+  font "MiSans TC/ttf/MisansTC-Medium.ttf"
+  font "MiSans TC/ttf/MisansTC-Thin.ttf"
+  font "MiSans TC/ttf/MisansTC-ExtraLight.ttf"
+  font "MiSans TC/ttf/MisansTC-Semibold.ttf"
+  font "MiSans TC/ttf/MisansTC-Heavy.ttf"
+  font "MiSans TC/ttf/MisansTC-Demibold.ttf"
+  font "MiSans TC/ttf/MisansTC-Normal.ttf"
+
+end
--- Casks/font-mi-sans.rb
+++ Casks/font-mi-sans.rb
@@ -12,9 +12,7 @@ cask "font-mi-sans" do
     cask "font-mi-sans"
   end
 
-  # MiSans (中文简体)
   font "MiSans/可变字体/MiSansVF.ttf"
-  # ttf
   font "MiSans/ttf/MiSans-Regular.ttf"
   font "MiSans/ttf/MiSans-Bold.ttf"
   font "MiSans/ttf/MiSans-Light.ttf"
@@ -25,38 +23,5 @@ cask "font-mi-sans" do
   font "MiSans/ttf/MiSans-Heavy.ttf"
   font "MiSans/ttf/MiSans-Demibold.ttf"
   font "MiSans/ttf/MiSans-Normal.ttf"
-  # # otf
-  # font "MiSans/otf/MiSans-Bold.otf"
-  # font "MiSans/otf/MiSans-Demibold.otf"
-  # font "MiSans/otf/MiSans-ExtraLight.otf"
-  # font "MiSans/otf/MiSans-Heavy.otf"
-  # font "MiSans/otf/MiSans-Light.otf"
-  # font "MiSans/otf/MiSans-Medium.otf"
-  # font "MiSans/otf/MiSans-Normal.otf"
-  # font "MiSans/otf/MiSans-Regular.otf"
-  # font "MiSans/otf/MiSans-Semibold.otf"
-  # font "MiSans/otf/MiSans-Thin.otf"
-  # # woff
-  # font "MiSans/woff/MiSans-Bold.woff"
-  # font "MiSans/woff/MiSans-Demibold.woff"
-  # font "MiSans/woff/MiSans-ExtraLight.woff"
-  # font "MiSans/woff/MiSans-Heavy.woff"
-  # font "MiSans/woff/MiSans-Light.woff"
-  # font "MiSans/woff/MiSans-Medium.woff"
-  # font "MiSans/woff/MiSans-Normal.woff"
-  # font "MiSans/woff/MiSans-Regular.woff"
-  # font "MiSans/woff/MiSans-Semibold.woff"
-  # font "MiSans/woff/MiSans-Thin.woff"
-  # # woff2
-  # font "MiSans/woff2/MiSans-Bold.woff2"
-  # font "MiSans/woff2/MiSans-Demibold.woff2"
-  # font "MiSans/woff2/MiSans-ExtraLight.woff2"
-  # font "MiSans/woff2/MiSans-Heavy.woff2"
-  # font "MiSans/woff2/MiSans-Light.woff2"
-  # font "MiSans/woff2/MiSans-Medium.woff2"
-  # font "MiSans/woff2/MiSans-Normal.woff2"
-  # font "MiSans/woff2/MiSans-Regular.woff2"
-  # font "MiSans/woff2/MiSans-Semibold.woff2"
-  # font "MiSans/woff2/MiSans-Thin.woff2"
 
 end
