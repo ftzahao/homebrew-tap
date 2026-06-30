b2dde4d848dcb27baf386f220c372466e5fbeddb
ftzahao <ftzahao@icloud.com>
2026-06-30 13:21:00 +0800

    还原 MiSans L3 字体版本至 1.000

--- Casks/font-mi-sans-l3.rb
+++ Casks/font-mi-sans-l3.rb
@@ -1,5 +1,5 @@
 cask "font-mi-sans-l3" do
-  version "4.003"
+  version "1.000"
   sha256 "467fe0171ec9ea21d925aba9c032b1a775a3e756c458f075e7a2ee57568b0c79"
 
   url "https://hyperos.mi.com/font-download/MiSans_L3.zip",
